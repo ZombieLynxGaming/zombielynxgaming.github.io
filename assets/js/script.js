a8fbbaa84f1d77c18a42ed1133174b3dc8b23388
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2023-08-25 02:00:47 -0500

    let's hope

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,159 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const ark1 = document.getElementById("ark-display");
+const ark2 = document.getElementById("ark-button");
+const eco1 = document.getElementById("eco-display");
+const eco2 = document.getElementById("eco-button");
+const minecraft1 = document.getElementById("minecraft-display");
+const minecraft2 = document.getElementById("minecraft-button");
+const empyrion1 = document.getElementById("empyrion-display");
+const empyrion2 = document.getElementById("empyrion-button");
+const rust1 = document.getElementById("rust-display"); 
+const rust2 = document.getElementById("rust-button");
+const serverBkg = document.getElementById("servers");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-
-;(function ($) {
-
-	'use strict';
-
-
-	$('.portfolio-single-slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-
-	});
-
-	$('.clients-logo').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
-
-	$('.testimonial-wrap').slick({
-		slidesToShow: 2,
-		slidesToScroll: 2,
-		infinite: true,
-		dots: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 6000,
-		responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		        infinite: true,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 900,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 2
-		      }
-		    },{
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    }
-		  
-  		]
-	});
-
-
-   $('.portfolio-gallery').each(function () {
-        $(this).find('.popup-gallery').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
+function applyAnimation(element, animationClass) {
+    element.classList.add(animationClass);
+  
+    // Listen for the animation end event
+    element.addEventListener('animationend', function () {
+      element.classList.remove(animationClass);
     });
+  }
 
 
-	var map;
+function ark() {
+    // Change the images
+    ark1.classList?.remove("d-none");
+    eco1.classList?.add("d-none");
+    minecraft1?.classList.add("d-none");
+    empyrion1?.classList.add("d-none");
+    rust1?.classList.add("d-none");
+    console.log("ark executed"); 
+    // Hightlight button
+    ark2.setAttribute('style', 'border: 5px solid yellow');
+    eco2.setAttribute('style', 'border: 5px solid black');
+    minecraft2.setAttribute('style', 'border: 5px solid black');
+    empyrion2.setAttribute('style', 'border: 5px solid black');
+    rust2.setAttribute('style', 'border: 5px solid black');
+    // Change Background
+    serverBkg.classList?.remove("eco-bkg", "minecraft-bkg", "empyrion-bkg", "rust-bkg");
+    serverBkg.classList?.add("ark-bkg");
+    applyAnimation(ark1, 'slide-in-from-right');
+  }
 
-	function initialize() {
-		var mapOptions = {
-			zoom: 13,
-			center: new google.maps.LatLng(50.97797382271958, -114.107718560791)
-			// styles: style_array_here
-		};
-		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	}
+function eco() {
+    // Change the images
+    eco1?.classList.remove("d-none");
+    ark1?.classList.add("d-none");
+    minecraft1?.classList.add("d-none");
+    empyrion1?.classList.add("d-none");
+    rust1?.classList.add("d-none");
+    console.log("eco executed");  
+    // Hightlight button
+    eco2.setAttribute('style', 'border: 5px solid yellow');
+    ark2.setAttribute('style', 'border: 5px solid black');
+    minecraft2.setAttribute('style', 'border: 5px solid black');
+    empyrion2.setAttribute('style', 'border: 5px solid black');
+    rust2.setAttribute('style', 'border: 5px solid black'); 
+    // Change Background
+    serverBkg.classList?.remove("ark-bkg", "minecraft-bkg", "empyrion-bkg", "rust-bkg");
+    serverBkg.classList?.add("eco-bkg");
+      // Apply the animation to the eco-display element
+    applyAnimation(eco1, 'slide-in-from-right');
+}
 
-	var google_map_canvas = $('#map-canvas');
+function minecraft() {
+    // Change the images
+    minecraft1?.classList.remove("d-none");
+    ark1?.classList.add("d-none");
+    empyrion1?.classList.add("d-none");
+    eco1?.classList.add("d-none");
+    rust1?.classList.add("d-none");
+    console.log("empyrion executed");
+    // Hightlight button
+    minecraft2.setAttribute('style', 'border: 5px solid yellow');
+    ark2.setAttribute('style', 'border: 5px solid black');
+    eco2.setAttribute('style', 'border: 5px solid black');
+    empyrion2.setAttribute('style', 'border: 5px solid black');
+    rust2.setAttribute('style', 'border: 5px solid black'); 
+    // Change Background
+    serverBkg.classList?.remove("ark-bkg", "eco-bkg", "empyrion-bkg", "rust-bkg");
+    serverBkg.classList?.add("minecraft-bkg");   
+}
 
-	if (google_map_canvas.length) {
-		google.maps.event.addDomListener(window, 'load', initialize);
-	}
+function empyrion() {
+    // Change the images
+    empyrion1?.classList.remove("d-none");
+    ark1?.classList.add("d-none");
+    minecraft1?.classList.add("d-none");
+    eco1?.classList.add("d-none");
+    rust1?.classList.add("d-none");
+    console.log("empyrion executed");   
+    // Hightlight button
+    empyrion2.setAttribute('style', 'border: 5px solid yellow');
+    ark2.setAttribute('style', 'border: 5px solid black');
+    minecraft2.setAttribute('style', 'border: 5px solid black');
+    eco2.setAttribute('style', 'border: 5px solid black');
+    rust2.setAttribute('style', 'border: 5px solid black'); 
+    // Change Background
+    serverBkg.classList?.remove("ark-bkg", "eco-bkg", "minecraft-bkg", "rust-bkg");
+    serverBkg.classList?.add("empyrion-bkg");  
+}
 
-	// Counter
+function rust() {
+    // Change the images
+    rust1?.classList.remove("d-none");
+    ark1?.classList.add("d-none");
+    minecraft1?.classList.add("d-none");
+    eco1?.classList.add("d-none");
+    empyrion1?.classList.add("d-none");
+    console.log("empyrion executed");   
+    // Hightlight button
+    rust2.setAttribute('style', 'border: 5px solid yellow');
+    ark2.setAttribute('style', 'border: 5px solid black');
+    minecraft2.setAttribute('style', 'border: 5px solid black');
+    empyrion2.setAttribute('style', 'border: 5px solid black');
+    eco2.setAttribute('style', 'border: 5px solid black'); 
+    // Change Background
+    serverBkg.classList?.remove("ark-bkg", "eco-bkg", "minecraft-bkg", "empyrion-bkg");
+    serverBkg.classList?.add("rust-bkg");  
+}
 
-	$('.counter-stat').counterUp({
-	      delay: 10,
-	      time: 1000
-	  });
 
-	
+AOS.init();
+ 
 
-})(jQuery);
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const sectionId = entry.target.id;
+      const navLink = document.getElementById(`nav-${sectionId}`);
+      
+      if (entry.isIntersecting) {
+        navLink.classList.add("active");
+      } else {
+        navLink.classList.remove("active");
+      }
+    });
+  }, { threshold: 0.5 });
+  
+  const sections = document.querySelectorAll("section");
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+  
+  function scrollToServers(event) {
+    event.preventDefault();
+    const serversSection = document.getElementById("servers");
+    const navHeight = document.querySelector(".navtop").offsetHeight;
+    const yOffset = serversSection.getBoundingClientRect().top + window.pageYOffset - navHeight;
+    window.scrollTo({ top: yOffset, behavior: "smooth" });
+  }
+  
+  function scrollToAbout(event) {
+    event.preventDefault();
+    const aboutSection = document.getElementById("about");
+    const navHeight = document.querySelector(".navtop").offsetHeight;
+    const yOffset = aboutSection.getBoundingClientRect().top + window.pageYOffset - navHeight;
+    window.scrollTo({ top: yOffset, behavior: "smooth" });
+  }
+  
+
